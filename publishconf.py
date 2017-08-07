0c93fef0c8601c3c36fa30e10ce93f2842cebb0c
Valerian <vfreysz@gmail.com>
2017-08-07 12:03:23 +0200

    modification URL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://vfreysz.github.io'
+SITEURL = 'https://vfreysz.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
