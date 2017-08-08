c5b1f603ddea2c01545cc1832440ec2088d41861
Valerian <vfreysz@gmail.com>
2017-08-08 18:18:29 +0200

    utilisation de pure + nouveau pygments

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Valerian FREYSZ'
-SITENAME = u'ValsBlog'
-SITEURL = ''
 
+
+AUTHOR = 'Valerian FREYSZ'
+SITENAME = 'ValsBlog'
+SITEURL = ''
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
@@ -26,15 +27,37 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = 	(('github', 'https://github.com/vfreysz'),
+		('linkedin', 'https://www.linkedin.com/in/valerian-freysz'),)
 
-DEFAULT_PAGINATION = 10
+
+DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-MARKUP = ('md', 'ipynb')
 
-PLUGIN_PATHS = ['./plugins']
+#Plugin
+PLUGIN_PATHS = ['../pelican-plugins']
 PLUGINS = ['ipynb.markup']
+#PLUGINS = ['pelican-ipynb.markup','i18n_subsites']
+
+#Ipython notebook
+#MARKUP = ('md', )
+MARKUP = ('md', 'ipynb')
+IPYNB_IGNORE_CSS = True
+
+
+#JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+
+#THEME = u'../pelican-themes/pelican-bootstrap3'
+#THEME = u'../pelican-themes/SoMA'
+#THEME = u'../pelican-themes_git/pelican-pure'
+#THEME = u'../pelican-themes_git/pure-single'
+THEME = u'../pelican-themes_git/pure'
+PROFILE_IMG_URL = u'photo/photo_nb_large.jpg'
+COVER_IMG_URL = u'photo/Side_image_ld.jpg'
+
+
+
+
