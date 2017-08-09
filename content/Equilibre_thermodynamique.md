76478d25e22f045dbd741edf34340740a112e7c0
Valerian <vfreysz@gmail.com>
2017-08-09 18:29:39 +0200

    tout neuf

---
+++ content/Equilibre_thermodynamique.md
@@ -0,0 +1,10 @@
+Title: Evaluation de la composition de sortie d'un gazeifieur à l'équilibre thermodynamique. 
+Date: 2017-08-07 10:03
+Category: Gazeification
+Tags: python, thermodynamique, gazeification
+Authors: FREYSZ Valerian
+Summary: Dans cet article, nous développons un code sous Python pour évaluer l'influence de la pression et de la température sur les compositions de sortie d'un gazeifieur de biomasse/charbon. Nous verrons les bases théoriques de l'équilibre thermodynamique d'un système de réactions chimiques. Puis nous développerons un code de résolution numérique à partir de ces bases. Enfin, nous utiliserons ce code pour évaluer l'influence de la pression et de la température. 
+Slug: article-equilibre-gazeification
+
+{% notebook /home/valerian/vfreysz@gmail.com/Python_Git/Equilibre_Thermo/Equilibre_thermodynamique.ipynb cells[2:] %}
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Valerian FREYSZ'
-SITENAME = u'ValsBlog'
-SITEURL = ''
 
+
+AUTHOR = 'Valerian FREYSZ'
+SITENAME = 'ValsBlog'
+SITEURL = ''
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
@@ -20,16 +21,45 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = 	(('github', 'https://github.com/vfreysz'),
+		('linkedin', 'https://www.linkedin.com/in/valerian-freysz'),)
+
 
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+
+#Plugin
+PLUGIN_PATHS = ['../pelican-plugins']
+
+#PLUGINS = ['pelican-ipynb.markup','i18n_subsites']
+
+#Ipython notebook
+MARKUP = ('md', )
+PLUGINS = ['ipynb.liquid']
+#MARKUP = ('md', 'ipynb')
+#PLUGINS = ['ipynb.markup']
+#IPYNB_IGNORE_CSS = True
+
+
+#JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+
+#THEME = u'../pelican-themes/pelican-bootstrap3'
+#THEME = u'../pelican-themes/SoMA'
+#THEME = u'../pelican-themes_git/pelican-pure'
+#THEME = u'../pelican-themes_git/pure-single'
+THEME = u'../pelican-themes_git/pure'
+COVER_IMG_URL = u'https://github.com/vfreysz/vfreysz.github.io-src/raw/master/photo_site/Side_image_ld.jpg'
+PROFILE_IMG_URL = u'https://github.com/vfreysz/vfreysz.github.io-src/raw/master/photo_site/photo_nb_large.jpg'
+
+
+
+
